9aa9569a51b5b10093526d0a4ae289011ed90d28
u-235 <nicegorov@yandex.ru>
2021-09-19 18:48:45 +0300

    Create the file cmake/get-version.cmake
    cmake/get-version.cmake gets the current version number from
    src/version.h and sets the PROJECT_VERSION_*** variables.
    
    The features used are available from cmake 3.9.

---
+++ cmake/get-version.cmake
@@ -0,0 +1,30 @@
+# Getting the application version from the version.h file.
+# The result is saved in the PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR,
+# PROJECT_VERSION_PATCH and PROJECT_VERSION variables.
+# CMAKE_MATCH_n request CMake 3.9
+
+cmake_minimum_required(VERSION 3.9)
+
+file(READ ${CMAKE_SOURCE_DIR}/src/version.h VERSION_H)
+
+string(REGEX MATCH "#define[ \t]*APP_VERSION_MAJOR[ \t]*([0-9]*)" _ ${VERSION_H})
+if (CMAKE_MATCH_1)
+    set(PROJECT_VERSION_MAJOR ${CMAKE_MATCH_1})
+    set(PROJECT_VERSION ${CMAKE_MATCH_1})
+else ()
+    return()
+endif ()
+
+string(REGEX MATCH "#define[ \t]*APP_VERSION_MINOR[ \t]*([0-9]*)" _ ${VERSION_H})
+if (CMAKE_MATCH_1)
+    set(PROJECT_VERSION_MINOR ${CMAKE_MATCH_1})
+    string(APPEND PROJECT_VERSION .${CMAKE_MATCH_1})
+else ()
+    return()
+endif ()
+
+string(REGEX MATCH "#define[ \t]*APP_VERSION_PATCH[ \t]*([0-9]*)" _ ${VERSION_H})
+if (CMAKE_MATCH_1)
+    set(PROJECT_VERSION_PATCH ${CMAKE_MATCH_1})
+    string(APPEND PROJECT_VERSION .${CMAKE_MATCH_1})
+endif ()
