4864f8d389e2dd489ce1b48f311e1c0ea6986eb0
u-235 <nicegorov@yandex.ru>
2021-10-03 13:49:56 +0300

    Fix installation directories for KDE4
    For non-Windows systems, the BIN_INSTALL_DIR, XDG_APPS_INSTALL_DIR,
    ICON_INSTALL_DIR and LOCALE_INSTALL_DIR variables contain absolute
    paths, whereas Cpack requires relative paths for the installation
    folders.
    
    Furthermore, while BIN_INSTALL_DIR, XDG_APPS_INSTALL_DIR,
    ICON_INSTALL_DIR and LOCALE_INSTALL_DIR use the
    CMAKE_INSTALL_PREFIX prefix, the KDE4_BIN_INSTALL_DIR,
    KDE4_XDG_APPS_INSTALL_DIR, KDE4_ICON_INSTALL_DIR and
    KDE4_LOCALE_INSTALL_DIR variables always use the /usr prefix.

--- cmake/install-helper.cmake
+++ cmake/install-helper.cmake
@@ -12,9 +12,17 @@ set(APP_DEF_INSTALL_DIR    ${CMAKE_INSTALL_DATADIR}/applications)
 set(APP_ICONS_INSTALL_DIR  ${CMAKE_INSTALL_DATADIR}/icons)
 set(APP_LOCALE_INSTALL_DIR ${CMAKE_INSTALL_LOCALEDIR})
 
-if (${FRAMEWORK} STREQUAL KDE4)
-    set(APP_BIN_INSTALL_DIR    ${KDE4_BIN_INSTALL_DIR})
-    set(APP_DEF_INSTALL_DIR    ${KDE4_XDG_APPS_INSTALL_DIR})
-    set(APP_ICONS_INSTALL_DIR  ${KDE4_ICON_INSTALL_DIR})
-    set(APP_LOCALE_INSTALL_DIR ${LOCALE_INSTALL_DIR})
+# From https://cmake.org/cmake/help/latest/command/install.html
+#
+# As absolute paths are not supported by cpack installer generators, it is
+# preferable to use relative paths throughout. In particular, there is no need
+# to make paths absolute by prepending CMAKE_INSTALL_PREFIX; this prefix is
+# used by default if the DESTINATION is a relative path.
+#
+# But in FindKDE4Internal.cmake for unix uses absolute paths.
+if (${FRAMEWORK} MATCHES KDE)
+    string(REPLACE "${CMAKE_INSTALL_PREFIX}/" "" APP_BIN_INSTALL_DIR    ${BIN_INSTALL_DIR})
+    string(REPLACE "${CMAKE_INSTALL_PREFIX}/" "" APP_DEF_INSTALL_DIR    ${XDG_APPS_INSTALL_DIR})
+    string(REPLACE "${CMAKE_INSTALL_PREFIX}/" "" APP_ICONS_INSTALL_DIR  ${ICON_INSTALL_DIR})
+    string(REPLACE "${CMAKE_INSTALL_PREFIX}/" "" APP_LOCALE_INSTALL_DIR ${LOCALE_INSTALL_DIR})
 endif ()
