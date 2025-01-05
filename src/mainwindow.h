u-235/uchmviewer-draft

ViewWindowMgr: add the browserChanged signal

src/mainwindow.h
@@ -99,9 +99,6 @@ class MainWindow : public QMainWindow, public Ui::MainWindow
 		// also sends the command-line data there so it runs properly.
 		bool        hasSameTokenInstance();
 
-		// Called from WindowMgr when another browser tab is activated
-		void        browserChanged(ViewWindow* controller);
-
 		// Adds some main window actions to the provided popup menu
 		void        setupPopupMenu( QMenu* menu );
 
@@ -113,6 +110,9 @@ class MainWindow : public QMainWindow, public Ui::MainWindow
 		const QPixmap* getEBookIconPixmap( EBookTocEntry::Icon imagenum );
 
 	public slots:
+		// Called from WindowMgr when another browser tab is activated
+		void        browserChanged(ViewWindow* controller);
+
 		// Navigation toolbar icons
 		void        navSetBackEnabled( bool enabled );
 		void        navSetForwardEnabled( bool enabled );

src/viewwindowmgr.cpp
@@ -312,7 +312,7 @@ void ViewWindowMgr::onTabChanged( int newtabIndex )
 	try {
 		const TabData& tab = findTabData( newtabIndex );
 		emit historyChanged();
-		mainWindow->browserChanged( tab.controller );
+		emit browserChanged( tab.controller );
 		tab.widget->setFocus();
 	} catch ( const std::invalid_argument& ) {
 	}

src/viewwindowmgr.h
@@ -72,6 +72,7 @@ class ViewWindowMgr : public QWidget, public Ui::TabbedBrowser
 		void    applyBrowserSettings();
 
 	signals:
+		void    browserChanged(ViewWindow* controller);
 		/**
 		 * This signal is emitted when tabs are switched or when the url of
 		 * the ViewWindow changes.

eBookProjects/uChmViewer

src/mainwindow.h
@@ -99,9 +99,6 @@ class MainWindow : public QMainWindow, public Ui::MainWindow
 		// also sends the command-line data there so it runs properly.
 		bool        hasSameTokenInstance();
 
-		// Called from WindowMgr when another browser tab is activated
-		void        browserChanged(ViewWindow* browser);
-
 		// Adds some main window actions to the provided popup menu
 		void        setupPopupMenu( QMenu* menu );
 
@@ -113,6 +110,9 @@ class MainWindow : public QMainWindow, public Ui::MainWindow
 		const QPixmap* getEBookIconPixmap( EBookTocEntry::Icon imagenum );
 
 	public slots:
+		// Called from WindowMgr when another browser tab is activated
+		void        browserChanged(ViewWindow* browser);
+
 		// Navigation toolbar icons
 		void        navSetBackEnabled( bool enabled );
 		void        navSetForwardEnabled( bool enabled );

src/viewwindowmgr.cpp
@@ -323,7 +323,7 @@ void ViewWindowMgr::onTabChanged( int newtabIndex )
 	try {
 		const TabData& tab = findTabData( newtabIndex );
 		emit historyChanged();
-		mainWindow->browserChanged( tab.browser );
+		emit browserChanged( tab.browser );
 		tab.widget->setFocus();
 	} catch ( const std::invalid_argument& ) {
 	}

src/viewwindowmgr.h
@@ -73,6 +73,7 @@ class ViewWindowMgr : public QWidget, public Ui::TabbedBrowser
 		void    applyBrowserSettings();
 
 	signals:
+		void    browserChanged(ViewWindow* browser);
 		/**
 		 * This signal is emitted when tabs are switched or when the url of
 		 * the ViewWindow changes.