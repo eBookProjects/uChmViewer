u-235/uchmviewer-draft

ViewWindow: rename the openUrl and getOpenedPage methods

src/mainwindow.cpp
@@ -465,7 +465,7 @@ bool MainWindow::openPage( const QUrl& url, unsigned int flags )
 		vwnd->setZoomFactor( zoom );
 	}
 
-	if ( vwnd->openUrl (url) )
+	if ( vwnd->load (url) )
 	{
 		// Open all the tree items to show current item (if needed)
 		if ( (flags & OPF_CONTENT_TREE) != 0 )
@@ -514,14 +514,14 @@ void MainWindow::setTextEncoding( const QString& encoding )
 		}
 	}
 
-	// Because updateView() will call view->invalidate(), which clears the view->getOpenedPage(),
+	// Because updateView() will call view->invalidate(), which clears the view->url(),
 	// we have to make a copy of it.
-	QUrl url = currentBrowser()->getOpenedPage();
+	QUrl url = currentBrowser()->url();
 
 	// Regenerate the content and index trees
 	refreshCurrentBrowser();
 
-	currentBrowser()->openUrl( url );
+	currentBrowser()->load( url );
 }
 
 void MainWindow::closeFile( )
@@ -728,7 +728,7 @@ void MainWindow::onOpenPageInNewBackgroundTab( )
 
 void MainWindow::browserChanged(ViewWindow* newbrowser )
 {
-	m_navPanel->findUrlInContents( newbrowser->getOpenedPage() );
+	m_navPanel->findUrlInContents( newbrowser->url() );
 }
 
 bool MainWindow::event( QEvent* e )
@@ -1006,7 +1006,7 @@ void MainWindow::actionViewHTMLsource()
 		return;
 	}
 
-	QUrl page = currentBrowser()->getOpenedPage();
+	QUrl page = currentBrowser()->url();
 
 	if ( pConfig->m_advUseInternalEditor )
 	{
@@ -1095,7 +1095,7 @@ void MainWindow::navigatorVisibilityChanged( bool visible )
 
 void MainWindow::actionLocateInContentsTab()
 {
-	if ( m_navPanel->findUrlInContents( currentBrowser()->getOpenedPage() ) )
+	if ( m_navPanel->findUrlInContents( currentBrowser()->url() ) )
 		m_navPanel->setActive( NavigationPanel::TAB_CONTENTS );
 	else
 		statusBar()->showMessage( i18n( "Could not locate opened topic in content window"), 2000 );

src/viewwindowmgr.cpp
@@ -273,7 +273,7 @@ void ViewWindowMgr::restoreSettings( const Settings::viewindow_saved_settings_t&
 	for ( int i = 0; i < settings.size(); i++ )
 	{
 		ViewWindow* window = addNewTab( false );
-		window->openUrl( settings[i].url ); // will call setTabName()
+		window->load( settings[i].url ); // will call setTabName()
 		window->setScrollbarPosition( settings[i].scroll_y );
 		window->setZoomFactor( settings[i].zoom );
 	}
@@ -287,7 +287,7 @@ void ViewWindowMgr::saveSettings( Settings::viewindow_saved_settings_t& settings
 		for ( int i = 0; i < m_tabWidget->count(); i++ )
 		{
 			const TabData& tab = findTabData( i );
-			settings.push_back( Settings::SavedViewWindow( tab.controller->getOpenedPage().toString(),
+			settings.push_back( Settings::SavedViewWindow( tab.controller->url().toString(),
 			                                               tab.controller->getScrollbarPosition(),
 			                                               tab.controller->getZoomFactor()) );
 		}
@@ -324,7 +324,7 @@ void ViewWindowMgr::onUrlChanged(const QUrl&)
 
 void ViewWindowMgr::openNewTab()
 {
-	::mainWindow->openPage( current()->getOpenedPage(), MainWindow::OPF_NEW_TAB | MainWindow::OPF_CONTENT_TREE );
+	::mainWindow->openPage( current()->url(), MainWindow::OPF_NEW_TAB | MainWindow::OPF_CONTENT_TREE );
 }
 
 void ViewWindowMgr::activateWindow()
@@ -438,7 +438,7 @@ void ViewWindowMgr::onWindowContentChanged(ViewWindow* controller)
 
 void ViewWindowMgr::copyUrlToClipboard()
 {
-	QString url = current()->getOpenedPage().toString();
+	QString url = current()->url().toString();
 
 	if ( !url.isEmpty() )
 		QApplication::clipboard()->setText( url );

eBookProjects/uChmViewer

src/mainwindow.cpp
@@ -475,7 +475,7 @@ bool MainWindow::onLinkClicked( ViewWindow* browser, const QUrl& url, unsigned i
 		browser->setZoomFactor( zoom );
 	}
 
-	if ( browser->openUrl (url) )
+	if ( browser->load (url) )
 	{
 		// Open all the tree items to show current item (if needed)
 		if ( (flags & OPF_CONTENT_TREE) != 0 )
@@ -524,14 +524,14 @@ void MainWindow::setTextEncoding( const QString& encoding )
 		}
 	}
 
-	// Because updateView() will call view->invalidate(), which clears the view->getOpenedPage(),
+	// Because updateView() will call view->invalidate(), which clears the view->url(),
 	// we have to make a copy of it.
-	QUrl url = currentBrowser()->getOpenedPage();
+	QUrl url = currentBrowser()->url();
 
 	// Regenerate the content and index trees
 	refreshCurrentBrowser();
 
-	currentBrowser()->openUrl( url );
+	currentBrowser()->load( url );
 }
 
 void MainWindow::closeFile( )
@@ -738,7 +738,7 @@ void MainWindow::onOpenPageInNewBackgroundTab( )
 
 void MainWindow::browserChanged(ViewWindow* browser)
 {
-	m_navPanel->findUrlInContents( browser->getOpenedPage() );
+	m_navPanel->findUrlInContents( browser->url() );
 }
 
 bool MainWindow::event( QEvent* e )
@@ -1016,7 +1016,7 @@ void MainWindow::actionViewHTMLsource()
 		return;
 	}
 
-	QUrl page = currentBrowser()->getOpenedPage();
+	QUrl page = currentBrowser()->url();
 
 	if ( pConfig->m_advUseInternalEditor )
 	{
@@ -1105,7 +1105,7 @@ void MainWindow::navigatorVisibilityChanged( bool visible )
 
 void MainWindow::actionLocateInContentsTab()
 {
-	if ( m_navPanel->findUrlInContents( currentBrowser()->getOpenedPage() ) )
+	if ( m_navPanel->findUrlInContents( currentBrowser()->url() ) )
 		m_navPanel->setActive( NavigationPanel::TAB_CONTENTS );
 	else
 		statusBar()->showMessage( i18n( "Could not locate opened topic in content window"), 2000 );

src/viewwindowmgr.cpp
@@ -291,7 +291,7 @@ void ViewWindowMgr::restoreSettings( const Settings::viewindow_saved_settings_t&
 	for ( int i = 0; i < settings.size(); i++ )
 	{
 		ViewWindow* browser = addNewTab( false );
-		browser->openUrl( settings[i].url ); // will call setTabName()
+		browser->load( settings[i].url ); // will call setTabName()
 		browser->setScrollbarPosition( settings[i].scroll_y );
 		browser->setZoomFactor( settings[i].zoom );
 	}
@@ -305,7 +305,7 @@ void ViewWindowMgr::saveSettings( Settings::viewindow_saved_settings_t& settings
 		for ( int i = 0; i < m_tabWidget->count(); i++ )
 		{
 			const TabData& tab = findTabData( i );
-			settings.push_back( Settings::SavedViewWindow( tab.browser->getOpenedPage().toString(),
+			settings.push_back( Settings::SavedViewWindow( tab.browser->url().toString(),
 			                                               tab.browser->getScrollbarPosition(),
 			                                               tab.browser->getZoomFactor()) );
 		}
@@ -353,7 +353,7 @@ void ViewWindowMgr::onBrowserLoadFinished(ViewWindow* browser, bool success)
 
 void ViewWindowMgr::openNewTab()
 {
-	::mainWindow->openPage( current()->getOpenedPage(), MainWindow::OPF_NEW_TAB | MainWindow::OPF_CONTENT_TREE );
+	::mainWindow->openPage( current()->url(), MainWindow::OPF_NEW_TAB | MainWindow::OPF_CONTENT_TREE );
 }
 
 void ViewWindowMgr::activateWindow()
@@ -462,7 +462,7 @@ void ViewWindowMgr::onFindPrevious()
 
 void ViewWindowMgr::copyUrlToClipboard()
 {
-	QString url = current()->getOpenedPage().toString();
+	QString url = current()->url().toString();
 
 	if ( !url.isEmpty() )
 		QApplication::clipboard()->setText( url );